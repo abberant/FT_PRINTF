636429e2ab47d128861c9f7681339a789ba0d076
abberant <109932381+abberant@users.noreply.github.com>
2022-11-20 22:43:40 +0100

    update
    format to layout

--- ft_printf/ft_format.c
+++ ft_printf/ft_format.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_format.c                                        :+:      :+:    :+:   */
+/*   ft_layout.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aanouari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,27 +12,27 @@
 
 #include "ft_printf.h"
 
-int	ft_format(const char *str_format, va_list arg)
+int	ft_layout(const char *str, va_list arg)
 {
 	int	len;
 
 	len = 0;
-	if (*str_format == '%')
+	if (*str == '%')
 		len += ft_putchar('%');
-	if (*str_format == 'c')
+	if (*str == 'c')
 		len += ft_putchar(va_arg(arg, int));
-	if (*str_format == 's')
+	if (*str == 's')
 		len += ft_putstr(va_arg(arg, char *));
-	if (*str_format == 'd' || *str_format == 'i')
+	if (*str == 'd' || *str == 'i')
 		len += ft_putnbr(va_arg(arg, int));
-	if (*str_format == 'u')
+	if (*str == 'u')
 		len += ft_putunbr(va_arg(arg, unsigned int));
-	if (*str_format == 'x' || *str_format == 'X')
-		len += ft_puthexnbr(va_arg(arg, unsigned int));
-	if (*str_format == 'p')
+	if (*str == 'X' || *str == 'x')
+		len += ft_puthexnbr(va_arg(arg, unsigned int), *str == 'X');
+	if (*str == 'p')
 	{
 		len += ft_putstr("0x");
-		len += ft_puthexnbr(va_arg(arg, unsigned long));
+		len += ft_puthexnbr(va_arg(arg, unsigned long), 0);
 	}
 	return (len);
 }
